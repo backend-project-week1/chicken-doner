e1cb9bb465aab1cfa95eae1193ea8b4bfd65bd98
no.oneho <oneho.dev@gmail.com>
2023-08-08 16:10:13 +0900

    로그4j 세팅

--- src/main/java/com/supercoding/chickendoner/controller/TestController.java
+++ src/main/java/com/supercoding/chickendoner/controller/TestController.java
@@ -5,11 +5,13 @@ import com.supercoding.chickendoner.common.util.ApiUtils;
 import com.supercoding.chickendoner.service.ErrorExampleService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class TestController {
 
     private final ErrorExampleService errorExampleService;
