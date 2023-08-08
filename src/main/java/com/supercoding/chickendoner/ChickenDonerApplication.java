9364630ac167de1e3977bc65fd13739c7e0b9d53
no.oneho <oneho.dev@gmail.com>
2023-08-08 16:10:13 +0900

    로그4j 세팅

--- src/main/java/com/supercoding/chickendoner/ChickenDonerApplication.java
+++ src/main/java/com/supercoding/chickendoner/ChickenDonerApplication.java
@@ -1,5 +1,7 @@
 package com.supercoding.chickendoner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChickenDonerApplication {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ChickenDonerApplication.class);
         SpringApplication.run(ChickenDonerApplication.class, args);
     }
 
--- src/main/java/com/supercoding/chickendoner/service/ErrorExampleService.java
+++ src/main/java/com/supercoding/chickendoner/service/ErrorExampleService.java
@@ -2,9 +2,11 @@ package com.supercoding.chickendoner.service;
 
 import com.supercoding.chickendoner.common.Error.CustomException;
 import com.supercoding.chickendoner.common.Error.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ErrorExampleService {
 
     public void simulateError1() {
@@ -17,6 +19,7 @@ public class ErrorExampleService {
     }
     public void simulateError3() {
         // 여기서 에러를 발생시키는 시뮬레이션을 수행합니다.
+        log.error(ErrorCode.USERNAME_DUPLICATION.getMessage());
         throw new CustomException(ErrorCode.USERNAME_DUPLICATION);
     }
 }
