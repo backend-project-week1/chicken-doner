9976793a8db7f29efe8f3587417d2e417357ef44
김주환 <kjh71047@naver.com>
2023-08-09 00:26:47 +0900

    ErrorCode 공백 제거

--- src/main/java/com/supercoding/chickendoner/common/Error/ErrorCode.java
+++ src/main/java/com/supercoding/chickendoner/common/Error/ErrorCode.java
@@ -27,23 +27,6 @@ public enum ErrorCode {
     NOTFOUND_POST(404, "해당 게시글이 존재하지 않습니다."),
     CONVERTING_FAILED(400, "파일 변환에 실패했습니다."),
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // 치킨
     NOTFOUND_ALL_CHICKEN(404,"현재 등록된 치킨이 없습니다."),
     NOTFOUND_CHICKEN(404,"해당 치킨은 존재하지 않습니다.")
