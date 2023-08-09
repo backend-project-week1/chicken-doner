75ff190711a20239aa98761f71fc4ced93504ba8
mahmunsen <tjs339@gmail.com>
2023-08-09 23:42:48 +0900

    마이페이지에서 장바구니 리스트 조회 기능 수정

--- src/main/java/com/supercoding/chickendoner/dto/response/ChickenScrap.java
+++ src/main/java/com/supercoding/chickendoner/dto/response/ChickenScrap.java
@@ -1,5 +1,6 @@
 package com.supercoding.chickendoner.dto.response;
 
+import com.supercoding.chickendoner.entity.Scrap;
 import lombok.*;
 
 @Getter
@@ -9,9 +10,23 @@ import lombok.*;
 @Builder
 public class ChickenScrap {
 
+    private Long id;
     private String chickenName;
     private String content;
     private Long price;
+    private Long amount;
+
+
+    public  ChickenScrap toChickenScrap(Scrap scrap){
+        return ChickenScrap.builder()
+                .chickenName(scrap.getChicken().getChikenName()) // 치킨 엔티티의 이름 필드를 가져와 설정
+                .content(scrap.getChicken().getContent())   // 치킨 엔티티의 content 필드를 가져와 설정
+                .price(scrap.getChicken().getPrice())       // 치킨 엔티티의 price 필드를 가져와 설정
+                .id(scrap.getChicken().getId())
+                .amount(scrap.getAmount())
+                .build();
+
+    }
 
 }
 
