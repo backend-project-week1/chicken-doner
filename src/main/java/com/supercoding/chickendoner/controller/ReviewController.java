6f42f77d927c028541284b1d6788c795319d7f9a
jungryuel <zldrktduf0927@naver.com>
2023-08-10 00:39:15 +0900

    리뷰 CRUD 입니다

--- src/main/java/com/supercoding/chickendoner/controller/ReviewController.java
+++ src/main/java/com/supercoding/chickendoner/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.supercoding.chickendoner.controller;
 
 import com.supercoding.chickendoner.common.CommonResponse;
 import com.supercoding.chickendoner.common.util.ApiUtils;
+import com.supercoding.chickendoner.dto.request.ReviewCreateRequest;
 import com.supercoding.chickendoner.dto.response.ReviewResponse;
 import com.supercoding.chickendoner.entity.Review;
 
@@ -20,10 +21,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ReviewController {
 
@@ -31,7 +33,7 @@ public class ReviewController {
 
     @Auth
     @PostMapping("/review")
-    public CommonResponse<Object> createReview(@RequestBody ReviewRequest reviewRequest) {
+    public CommonResponse<Object> createReview(@RequestBody ReviewCreateRequest reviewRequest) {
         //토큰 검증
         Long userIdx = AuthHolder.getUserIdx();
         reviewService.createReview(userIdx, reviewRequest);
@@ -40,36 +42,38 @@ public class ReviewController {
     }
  //게시글 삭제
     @Auth
-    @DeleteMapping("/review{ReviewId}")
-    public CommonResponse<Object> deleteReview(@PathVariable Long Id) {
+    @DeleteMapping("/review/{reviewId}")
+    public CommonResponse<Object> deleteReview(@PathVariable("reviewId") Long id) {
 
         Long userIdx = AuthHolder.getUserIdx();
 
-        reviewService.deleteReview(userIdx,Id);
-            return ApiUtils.success(true, 200, "리뷰 삭제 성공", null);
+      Long  deletedReviewId = reviewService.deleteReview(userIdx,id);
+            return ApiUtils.success(true, 200, deletedReviewId + " 번 리뷰 삭제 성공", deletedReviewId);
         }
+    //게시글 수정
     @Auth
-    @PatchMapping("/review{ReviewId}")
-    public CommonResponse<Object> updateReview(@PathVariable Long Id,
+    @PatchMapping("/review/{reviewId}")
+    public CommonResponse<Object> updateReview(@PathVariable("reviewId") Long id,
         @RequestBody ReviewRequest reviewRequest) {
 
         //유저검증
         Long userIdx = AuthHolder.getUserIdx();
-        reviewService.updateReview(userIdx,Id);
+        reviewService.updateReview(userIdx,id);
         return ApiUtils.success(true, 200, "리뷰 수정 성공", null);
     }
 
+
         //상세리뷰 조회
-        @GetMapping("/{reviewId}")
-            public CommonResponse<Object> reviewDetail(@PathVariable Long Id){//리뷰 Id로 조회
-            Review review = reviewService.getReview(Id);
+        @GetMapping("/review/{reviewId}")
+            public CommonResponse<Object> reviewDetail(@PathVariable("reviewId")Long id){//리뷰 Id로 조회
+            Review review = reviewService.getReview(id);
             return ApiUtils.success(true, 200, "상세리뷰 가져오기 성공", review);
         }
 
         //리뷰 리스트 조회
-        @GetMapping("/reviewId")
-            public CommonResponse<Object> reviewList(@RequestBody String Type){
-        List<ReviewResponse> responses = reviewService.getReviewList(Type);
+        @GetMapping("/review")
+            public CommonResponse<Object> reviewList(@RequestParam (required = false) String type){
+        List<ReviewResponse> responses = reviewService.getReviewList(type);
 
         if (responses != null){
             return ApiUtils.success(true,200,"리뷰목록 가져오기 성공",responses);
