1c3267c9037646fd450d89fec3e23e697cb80f6e
no.oneho <oneho.dev@gmail.com>
2023-08-09 11:09:05 +0900

    댓글 컨트롤러 클래스명 오탈자 수정

--- src/main/java/com/supercoding/chickendoner/controller/CommentController.java
+++ src/main/java/com/supercoding/chickendoner/controller/CommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
-public class CommentControllor {
+public class CommentController {
 
     private CommentService commentService;
 
