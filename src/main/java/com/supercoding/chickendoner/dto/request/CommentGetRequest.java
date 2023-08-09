7a926b4b5f92f5a3a97e52ed1d600ea7405d6e07
김주환 <kjh71047@naver.com>
2023-08-09 19:09:40 +0900

    comment 조회기능 완료

--- src/main/java/com/supercoding/chickendoner/dto/request/CommentGetRequest.java
+++ src/main/java/com/supercoding/chickendoner/dto/request/CommentGetRequest.java
@@ -7,6 +7,12 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CommentGetRequest {
-
     private Long reviewIdx;
+
+    public CommentGetRequest getCommentsGetByReview(Long reviewIdx) {
+
+        return CommentGetRequest.builder()
+                .reviewIdx(reviewIdx)
+                .build();
+    }
 }
